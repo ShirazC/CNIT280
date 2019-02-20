ebe73236ed57d057ce3d8589da619006c2bb5809
ShirazC <shiraz.chokshi@gmail.com>
2019-02-20 19:24:59 +0000

    finished templating, ready to add functional requirements

--- app.py
+++ app.py
@@ -4,7 +4,22 @@ app.config["DEBUG"] = True
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    title = "Epic Tutorials"
+    paragraph = ["wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!","wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!"]
+
+    try:
+        return render_template("index.html", title = title, paragraph=paragraph)
+    except Exception as e:
+        return str(e)
+
+# @app.route("/home/")
+# def home():
+#     return("Welcome Back")
+
+@app.route("/home/<string:name>/")
+def getUser(name):
+    return render_template("user.html", name=name)
+
 
 if __name__ == '__main__':
     app.run()
