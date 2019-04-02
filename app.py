f93d0054c248f95c04d8f9dac0b4f4eabf39ce69
Michael Borders <michael.a.borders@gmail.com>
2019-04-02 13:09:29 -0400

    Reformatted most of the program
    Fixed various PEP 8 errors, not all of them, some lines are still over
    79 characters.

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ from flask import Flask, render_template
 import flask_login
 from flask_login import current_user
 
-app = Flask(__name__)
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
-app.secret_key = os.urandom(12)
-accounts = json.load(open('users.json'))
-invoices = json.load(open('invoice.json'))
+
+APP = Flask(__name__)
+LOGIN_MANAGER = flask_login.LoginManager()
+LOGIN_MANAGER.init_app(APP)
+APP.secret_key = os.urandom(12)
+ACCOUNTS = json.load(open('users.json'))
+INVOICES = json.load(open('invoice.json'))
 # General TODOs
 # TODO: Create fake data for the second req we did for sprint 1
 # TODO: Requirements 3 & 4
@@ -22,63 +23,54 @@ class User(flask_login.UserMixin):
     pass
 
 
-@login_manager.user_loader
+@LOGIN_MANAGER.user_loader
 def user_loader(user_id):
-    if user_id not in accounts:
+    if user_id not in ACCOUNTS:
         return
     user = User()
     user.id = user_id
     return user
 
 
-@login_manager.request_loader
+@LOGIN_MANAGER.request_loader
 def request_loader(request):
     email = request.form.get('email')
-    if email not in accounts:
+    if email not in ACCOUNTS:
         return
     user = User()
     user.id = email
-    user.is_authenticated = request.form['password'] == accounts[email]['password']
+    user.is_authenticated = request.form['password'] == ACCOUNTS[email]['password']
     return user
 
 
 # TODO: Handle different user types correctly
-@app.route("/")
-def home(user=''):
-
-
-    if current_user.is_authenticated == False:
+@APP.route("/")
+def home():
+    if current_user.is_authenticated is False:
         # session['anonymous_user_id'] = user.id
         return render_template('login.html')
-    elif current_user.is_authenticated == True:
-        role=accounts[flask_login.current_user.id]["role"]
-        # role = ''
-        if role == 'Manager':
-            return render_template('users.html', user='Manager', )
-        elif role == 'Employee':
-            return render_template('users.html', user='Employee')
-        elif role == 'Customer':
-            return render_template('users.html', user='Customer')
-        # Redirect to the appropriate page here
-        else:
-            return 'Blah <a href="/logout">' + role + '</a>'
-    else:
-        return "You're retarded"
+    role = ACCOUNTS[flask_login.current_user.id]["role"]
 
+    if not role:
+        return 'Blah <a href="/logout"> Youre name is: ' + role + '</a>'
 
+    return render_template('users.html', user=str(role))
 
 
 
 # TODO: Handle the correct user types
-# TODO: Update users.json to use the correct user types. Placeholders work for now.
-@app.route('/login', methods=['GET', 'POST'])
+# TODO: Update users.json to use the correct user types instead of placeholders.
+@APP.route('/login', methods=['GET', 'POST'])
 def login():
     if flask.request.method == 'GET':
         return render_template('login.html')
+
     email = flask.request.form['username']
-    if email not in accounts:
+
+    if email not in ACCOUNTS:
         return render_template('login.html', error='Invalid credentials')
-    if flask.request.form['password'] == accounts[email]['password']:
+
+    if flask.request.form['password'] == ACCOUNTS[email]['password']:
         user = User()
         user.id = email
         flask_login.login_user(user, remember=False, duration=timedelta(seconds=5))
@@ -87,66 +79,59 @@ def login():
     return render_template('login.html', error='Invalid credentials')
 
 
-@app.route('/protected')
+@APP.route('/protected')
 @flask_login.login_required
 def protected():
-    if current_user.is_authenticated == False:
-        # session['anonymous_user_id'] = user.id
+    if current_user.is_authenticated is False:
         return flask.redirect(flask.url_for('login'))
-    role=accounts[flask_login.current_user.id]["role"]
-    if role == 'Manager':
-        return render_template('users.html', user='Manager')
-    elif role == 'Employee':
-        return render_template('users.html', user='Employee')
-    elif role == 'Customer':
-        return render_template('users.html', user='Customer')
-    else:
-         return 'Blah <a href="/logout"> Youre name is: ' + role + '</a>'
+
+    role = ACCOUNTS[flask_login.current_user.id]["role"]
+
+    if not role:
+        return 'Blah <a href="/logout"> Youre name is: ' + role + '</a>'
+
+    return render_template('users.html', user=str(role))
+
 
 @flask_login.login_required
-@app.route('/account')
+@APP.route('/account')
 def account():
-    if current_user.is_authenticated == False:
-        # session['anonymous_user_id'] = user.id
+    if current_user.is_authenticated is False:
         return flask.redirect(flask.url_for('login'))
-    role=accounts[flask_login.current_user.id]["role"]
-    if role == 'Manager':
-        return render_template('accounts.html', user='Manager')
-    elif role == 'Employee':
-        return render_template('accounts.html', user='Employee')
-    elif role == 'Customer':
-        return render_template('accounts.html', user='Customer')
-    else:
-         return 'Blah <a href="/logout"> Youre name is: ' + role + '</a>'
+
+    role = ACCOUNTS[flask_login.current_user.id]["role"]
+
+    if not role:
+        return 'Blah <a href="/logout"> Youre name is: ' + role + '</a>'
+
+    return render_template('users.html', user=str(role))
+
 
 @flask_login.login_required
-@app.route('/invoice')
+@APP.route('/invoice')
 def invoice():
-    if current_user.is_authenticated == False:
-        # session['anonymous_user_id'] = user.id
+    if current_user.is_authenticated is False:
         return flask.redirect(flask.url_for('login'))
-    role=accounts[flask_login.current_user.id]["role"]
-    if role == 'Manager':
-        return render_template('invoice.html', user='Manager', labels=invoices)
-    elif role == 'Employee':
-        return render_template('invoice.html', user='Employee', labels=invoices)
-    elif role == 'Customer':
-        return render_template('invoice.html', user='Customer',labels=invoices)
-    else:
-         return 'Blah <a href="/logout"> Youre name is: ' + role + '</a>'
+
+    role = ACCOUNTS[flask_login.current_user.id]["role"]
+
+    if not role:
+        return 'Blah <a href="/logout"> Youre name is: ' + role + '</a>'
+
+    return render_template('users.html', user=str(role), labels=INVOICES)
 
 
-@app.route('/logout')
+@APP.route('/logout')
 def logout():
     flask_login.logout_user()
     return render_template('login.html', error='Logged out')
 
 
-@login_manager.unauthorized_handler
+@LOGIN_MANAGER.unauthorized_handler
 def unauthorized_handler():
     return flask.redirect(flask.url_for('login'))
 
 
 if __name__ == '__main__':
-    app.secret_key = os.urandom(12)
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    APP.secret_key = os.urandom(12)
+    APP.run(debug=True, host='127.0.0.1', port=5000)
